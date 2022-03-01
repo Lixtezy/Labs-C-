3bda87979ff9290fd7ff98ce2c9477543c6d54d3
Eduard <73366831+Lixtezy@users.noreply.github.com>
2022-03-01 10:54:56 +0300

    Add files via upload

---
+++ Labs8/Labs8.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include "Time.h"
+
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	Time time1 = Time(5, 10, 125);
+	time1.TimeDisplay();
+	Time time2 = Time(10, 120, 125);
+	time2.TimeDisplay();
+	Time time3;
+	time3 = time1.AddTime2(time2);
+	time3.TimeDisplay();
+	return 0;
+}
